9636db1c0c0126cda24679f89f3ddd15b1f868ed
Salil Gupta <salil.gupta323@gmail.com>
2013-10-19 17:58:47 -0500

    changed the index file to a php file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+require_once($_SERVER['DOCUMENT_ROOT'].'/model/database/tables/CongressDB.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+	<script src = '/views/javascript/bootstrap.min.js'></script>
+	<script src = '/views/javascript/jquery/jquery-1.10.2.js'></script>
+	<link type = 'text/css' rel = 'stylesheet' href = '/views/css/bootstrap.min.css'/>
+	<link type = 'text/css' rel = 'stylesheet' href = '/views/css/bootstrap-theme.min.css'/>
+</head>
+<body>
+	<h1>OpShop</h1>
+</body>
+</html>
