444ac5204cd01a8aa944f5552275b4eb86d02a1d
Salil Gupta <salil.gupta323@gmail.com>
2013-10-19 17:57:39 -0500

    added the correct credentials for accessing the database

--- model/datastore/MySql/config.php
+++ model/datastore/MySql/config.php
@@ -1,8 +1,8 @@
 <?php
-define('_EVENT_LOGIN_', '_EVENT_LOGIN_');
-
+ini_set('display_errors',1);
+error_reporting(E_ALL);
 
 define ('OPSHOP_DB_HOST', 'localhost');
-define ('OPSHOP_DB_USER','USERNAME');//we'll have to figure this out...
-define ('OPSHOP_DB_PWD','PWD');//we'll have to figure this out...
-define ('OPSHOP_DB_NAME','opShop');
+define ('OPSHOP_DB_USER','root');//we'll have to figure this out...
+define ('OPSHOP_DB_PWD','root');//we'll have to figure this out...
+define ('OPSHOP_DB_NAME','opShop_dev');
