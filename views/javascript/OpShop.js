aab5bd6db1cf28f2067ea78097cdfb2baf28991b
Salil Gupta <salil.gupta323@gmail.com>
2013-10-29 17:26:05 -0500

    This is also not complete but this will be necessary for building the front end

--- views/javascript/OpShop.js
+++ views/javascript/OpShop.js
@@ -2,29 +2,78 @@ var OpShop = (function($){
 
 	analyzeArticle = function(link)
 	{
-		console.log(link);
-		$.ajax({
-			type :'get',
+		/*$.ajax({
+			type :"POST",
 			data:{
-				articleLink:link
+				link : link,
+				test : 'Salil'
 			},
-			url:'/controllers/OpShop.php',
+			url:'OpShopDriver.php',
 			datatype: 'json',
-			success:function(data)
+			success : function(data)
 			{
 				console.log(data);
-				alert('inputted url was correct');
+				alert(data);
 			},
+			error : function(xhr,status,error)
+			{
+				alert("Status: " + textStatus); alert("Error: " + errorThrown);
+				console.log(xhr);
+				console.log(status);
+				console.log(error);
+			}
+		});*/
 
+		$.getJSON('OpShopDriver.php',{link:link},function(data){
+			console.log(data);
+			$('.basic-info').show();
+			traverse(data);
 		});
 	}
 	init = function()
 	{
-		var articleLink = $("#articleLink").val();
-		$('#submit').on('click', function(){
+		$('.basic-info').hide();
+		$('#submit').click(function(){
+			var articleLink = $("#articleLink").val();
+			if(articleLink === '')
+				alert('error fill you need to input a url of an article');
 			analyzeArticle(articleLink);
+			return false;
 		});
 	}
-	retrieveOutput = function() {}
-	
+	traverse = function(data)
+	{
+		for(var el in data)
+		{
+			if(el === 'title')
+				$('.'+el+ '>div').append(data[el]==''?'Not Identified':data[el]);
+			else if (el === 'author')
+				$('.'+el+ '>div').append(data[el]==''?'Not Identified':data[el]);
+			else
+			{
+				var array = recurTraverse(data[el],[]);
+				$('.'+el+'> div').append(array.join(','));
+			}
+		}
+	}
+	recurTraverse = function(element,arr)
+	{
+		for(var el in element)
+		{
+			if(typeof element[el] === 'object')
+				recurTraverse(element[el],arr);
+			else
+			{
+				arr.push("<span>"+element[el]+"</span>");
+			}
+		}
+		return arr;
+	}
+	return {
+		//public functions
+		init: function()
+		{
+			return init();
+		}
+	};
 }(jQuery));
