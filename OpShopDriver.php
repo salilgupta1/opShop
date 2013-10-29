fc1e9a718054d577241f151b3f35876e6e07db12
Salil Gupta <salil.gupta323@gmail.com>
2013-10-29 17:44:10 -0500

    Initial code for the retrieval of information from the article using alchemy api

--- OpShopDriver.php
+++ OpShopDriver.php
@@ -5,14 +5,19 @@ require_once('controllers/TwitterApiController.php');
 require_once('controllers/AlchemyApiController.php');
 require_once('controllers/CongressDBController.php');
 
-$articleLink = isset($_GET['articleLink'])? $_GET['articleLink']?die('error');
+$articleLink = isset($_GET['link'])? $_GET['link']: die('error');
 
 $alchemyController = new AlchemyApiController($articleLink);
-$congressDB = new CongressDBController();
+//die('got here');
+//$congressDB = new CongressDBController();
 
-
-///do alchemy stuff here
-$people = $alchemyController->getEntities();
 $author = $alchemyController->getAuthor();
+$persons = $alchemyController->getEntities();
 $title = $alchemyController->getTitle();
+
 $keyWords = $alchemyController->getKeyWords();
+$concepts = $alchemyController->getConcept();
+
+$returnArray = array('title'=>$title,'author'=>$author,'keywords'=>$keyWords,'concepts'=>$concepts,'entities'=>$persons);
+
+echo json_encode($returnArray);
