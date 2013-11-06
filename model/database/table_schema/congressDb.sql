4aa9a71a8ea3ff2534c2b1e5035f2c2e9541212a
Salil Gupta <salil.gupta323@gmail.com>
2013-11-05 20:22:49 -0600

    put the new sql query to create the new table structure

--- model/database/table_schema/congressDb.sql
+++ model/database/table_schema/congressDb.sql
@@ -1,7 +1,13 @@
 CREATE TABLE `congress_db` (
   `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`pid`),
-  KEY `update` (`update`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `party` char(1) DEFAULT NULL,
+  `special` char(1) DEFAULT NULL,
+  `state` varchar(32) DEFAULT NULL,
+  `twitter_handle` varchar(64) DEFAULT NULL,
+  `image_url` varchar(255) DEFAULT NULL,
+  `position` char(1) DEFAULT NULL,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=102 ;
