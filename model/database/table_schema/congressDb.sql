7b63ff9e6b42312aab2f82b576c97b214c6d4c2b
alexesanz <alexesanz@gmail.com>
2013-11-11 01:24:58 -0600

    AES updated congressDB.php

--- model/database/table_schema/congressDb.sql
+++ model/database/table_schema/congressDb.sql
@@ -1,7 +1,14 @@
 CREATE TABLE `congress_db` (
   `pid` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL,
-  `update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`pid`),
-  KEY `update` (`update`)
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `rating` int(11) NOT NULL,
+  `party` char(1),
+  `special` char(1),
+  `state` varchar(32),
+  `twitter_handle` varchar(64),
+  `image_url` varchar(255),
+  `position` char(1),
+
+  PRIMARY KEY (`pid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8
