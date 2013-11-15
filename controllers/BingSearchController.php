eaae043552e159631c8a3f2dec427af48947758c
Salil Gupta <salil.gupta323@gmail.com>
2013-11-15 16:45:00 -0600

    Class that controls the bing search engine programmatically and return article urls

---
+++ controllers/BingSearchController.php
@@ -0,0 +1,55 @@
+<?php
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+class BingSearchController {
+
+	private $acctKey;
+	private $serviceOp;
+	private $top;
+
+	function __construct()
+	{
+		require($_SERVER['DOCUMENT_ROOT']."/assets/bingAccountKey.php");
+		$this->acctKey = $BING_SEARCH_ACCOUNT_KEY;
+		$this->serviceOp = "News";
+		$this->top = 3;
+	}
+
+	public function queryBing($query){
+		$rootUri = "https://api.datamarket.azure.com/Bing/Search";
+		//$query = urlencode($query);
+		$query = urlencode("'$query'");
+		//die($query);
+		$requestUri = "$rootUri/News?\$format=json&Query=$query";
+		$requestUri = $rootUri."/".$this->serviceOp."?\$format=json&\$top=".$this->top."&Query=".$query;
+		// Encode the credentials and create the stream context.
+		$auth = base64_encode("$this->acctKey:$this->acctKey");
+		$data = array(
+			'http' => array(
+			'request_fulluri' => true,
+			// ignore_errors can help debug – remove for production. This option added in PHP 5.2.10
+			'ignore_errors' => true,
+			'header' => "Authorization: Basic $auth")
+		);
+		$context = stream_context_create($data);
+		// Get the response from Bing.
+		$response = file_get_contents($requestUri, 0, $context);
+		return $response;
+	}
+
+	public function getArticleUrls($response){
+		$jsonObj = json_decode($response,true);
+		//print_r($jsonObj);
+		$urls = array();
+		foreach($jsonObj['d']['results'] as $value)
+		{
+			//print_r($value);
+			array_push($urls,$value['Url']);
+		}
+		//print_r($urls);
+		return $urls;
+	}
+
+
+
+}
