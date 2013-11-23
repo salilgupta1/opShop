82b2ec3bb9b97db8683b609748355852cb3f7278
Salil Gupta <salil.gupta323@gmail.com>
2013-11-23 16:20:53 -0600

    Changed the queries to include relevant information

--- controllers/CongressDBController.php
+++ controllers/CongressDBController.php
@@ -31,49 +31,46 @@ class CongressDBController {
 		return $result;
 	}*/
 
-	
+
 	public function findCongressPeopleInDB(){
-		
+
 		// Far liberal
-		//$f_lib_count = $this->congressDB->getCountOfCongressMenByRating(0);
-		//print $f_lib_count[0]["count(*)"];
 		$f_lib_rand = rand(0, self::F_LIB_COUNT-1);
-		//print $f_lib_rand." FLIB   ";
-		//print $f_lib_rand;
 		$f_lib_q = $this->congressDB->getCongressMenByRating(0, $f_lib_rand);
-		//print self::F_LIB_COUNT;
-		//print_r($f_lib_q);
-		//die();
-		//print_r($f_lib_q);
 		$f_lib_first_name = $f_lib_q[0]['first_name'];
 		$f_lib_last_name = $f_lib_q[0]['last_name'];
-		
+		$f_lib_state = $f_lib_q[0]['state'];
+		$f_lib_party = $f_lib_q[0]['party'];
+		$f_lib_pos = $f_lib_q[0]['position'];
+
 		// Moderate liberal
-		//$m_lib_count = $this->congressDB->getCountOfCongressMenByRating(1);
 		$m_lib_rand = rand(0, self::M_LIB_COUNT-1);
-		//print $m_lib_rand." MLIB   ";
 		$m_lib_q = $this->congressDB->getCongressMenByRating(1, $m_lib_rand);
-		//print_r($m_lib_q);
 		$m_lib_first_name = $m_lib_q[0]['first_name'];
 		$m_lib_last_name = $m_lib_q[0]['last_name'];
+		$m_lib_state = $m_lib_q[0]['state'];
+		$m_lib_party = $m_lib_q[0]['party'];
+		$m_lib_pos = $m_lib_q[0]['position'];
 
 		// Moderate conservative
-		//$m_con_count = $this->congressDB->getCountOfCongressMenByRating(2);
 		$m_con_rand = rand(0, self::M_CON_COUNT-1);
-		//print $m_con_rand." MCON   ";
 		$m_con_q = $this->congressDB->getCongressMenByRating(2, $m_con_rand);
 		$m_con_first_name = $m_con_q[0]['first_name'];
 		$m_con_last_name = $m_con_q[0]['last_name'];
+		$m_con_state = $m_con_q[0]['state'];
+		$m_con_party = $m_con_q[0]['party'];
+		$m_con_pos = $m_con_q[0]['position'];
 
 		// Far conservative
-		//$f_con_count = $this->congressDB->getCountOfCongressMenByRating(3);
 		$f_con_rand = rand(0, self::F_CON_COUNT-1);
-		//print $f_con_rand." FCON   ";
 		$f_con_q = $this->congressDB->getCongressMenByRating(3, $f_con_rand);
 		$f_con_first_name = $f_con_q[0]['first_name'];
 		$f_con_last_name = $f_con_q[0]['last_name'];
+		$f_con_state = $f_con_q[0]['state'];
+		$f_con_party = $f_con_q[0]['party'];
+		$f_con_pos = $f_con_q[0]['position'];
 
-		return array($f_lib_first_name." ".$f_lib_last_name, $m_lib_first_name." ".$m_lib_last_name, $m_con_first_name." ".$m_con_last_name, $f_con_first_name." ".$f_con_last_name);
+		return array($f_lib_first_name." ".$f_lib_last_name, $f_lib_state, $f_lib_party, $f_lib_pos, $m_lib_first_name." ".$m_lib_last_name, $m_lib_state, $m_lib_party, $m_lib_pos, $m_con_first_name." ".$m_con_last_name, $m_con_state, $m_con_party, $m_con_pos, $f_con_first_name." ".$f_con_last_name, $f_con_state, $f_con_party, $f_con_pos);
 
 	}//Alex build your function for choosing people in here. you can change the name
 }
