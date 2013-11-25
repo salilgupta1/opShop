b879965e969faef228c007ba32df36fa33153674
Salil Gupta <salil.gupta323@gmail.com>
2013-11-25 11:52:51 -0600

    Completed javascript

--- views/javascript/OpShop.js
+++ views/javascript/OpShop.js
@@ -6,19 +6,58 @@ var OpShop = (function($){
 		$.getJSON('OpShopDriver.php',{link:link},function(data){
 			console.log(data);
 			$('#loader').hide();
-			$('.basic-info').fadeIn('slow');
-			traverse(data);
+
+			var foundQuotes = populateCards(data);
+			if(foundQuotes)
+				$('#congyQuotes').fadeIn('slow');
+			else
+				$('#fail').fadeIn('slow');
+			//$('.basic-info').fadeIn('slow');
+			//traverse(data);*/
 		});
 	},
 	init = function()
 	{
 		$('#submit').click(function(){
+			$('#fail').hide();
+			$('#congyQuotes').hide();
 			var articleLink = $("#articleLink").val();
 			if(articleLink === '')
 				alert('error fill you need to input a url of an article');
-			analyzeArticle(articleLink);
+			else
+				analyzeArticle(articleLink);
 			return false;
 		});
+	},
+	populateCards = function(data){
+		if(data.length === 0)
+			return false;
+
+		$('.thumbnail').each(function(index){
+			var type = $(this).data('type');
+			if(data[type] !== undefined){
+				var name = data[type]['name'];
+				var url = data[type]['url'];
+				var pos = data[type]['pos'];
+				if(pos === 'S'){
+					pos = 'Senator';
+				}
+				else if(pos ==='H'){
+					pos = 'House of Rep.';
+				}
+				else {
+					pos = 'Other';
+				}
+				var party = data[type]['party'];
+				var state = data[type]['state'];
+				var quote = data[type]['quote']['quotation'];
+				$(this).children('.name').children('h3').text(name);
+				$(this).children('.position').children('h4').text(pos+ "("+party+" - "+state+")");
+				$(this).children('.quotation').children('p').text(quote);
+				$(this).children('a').attr('href',url);
+			}
+		});
+		return true;
 	},
 	traverse = function(data)
 	{
