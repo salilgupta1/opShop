4b7878a7bfb21d287a4552a4a60146351e8bb15c
Salil Gupta <salil.gupta323@gmail.com>
2013-11-26 19:33:51 -0600

    Tightened the key word relevancy to 0.89 from .75 to remove bad keywords

--- controllers/AlchemyApiController.php
+++ controllers/AlchemyApiController.php
@@ -48,9 +48,9 @@ class AlchemyApiController {
 		// so which keywords do we want... i think it should be the ones that are bigger than .9 or something in relevancez
 		foreach($keywords as $keyword)
 		{
-			if($keyword['relevance']>0.75)
+			if($keyword['relevance']>=0.88)
 			{
-				array_push($finalKeyWords,array('text'=>$keyword['text'] /*'relevance'=>$keyword['relevance']*/));
+				array_push($finalKeyWords,array('text'=>$keyword['text'], 'relevance'=>$keyword['relevance']));
 			}
 		}
 		return $finalKeyWords;
