6b87a31b01fdb5f95340d8685834a051c2eef162
Salil Gupta <salil.gupta323@gmail.com>
2013-12-07 15:47:31 -0600

    Just removed some unnecessary comments

--- model/datastore/MySql/config.php
+++ model/datastore/MySql/config.php
@@ -3,6 +3,6 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 define ('OPSHOP_DB_HOST', 'localhost');
-define ('OPSHOP_DB_USER','root');//we'll have to figure this out...
-define ('OPSHOP_DB_PWD','root');//we'll have to figure this out...
+define ('OPSHOP_DB_USER','root');
+define ('OPSHOP_DB_PWD','root');
 define ('OPSHOP_DB_NAME','opShop_dev');
